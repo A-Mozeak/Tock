50aebc1d6de7f6f99d28e4aac9b79339675a511f
Alex <admin@QAWSEDRFTGYHUJIKOLP.home>
2016-03-30 23:02:25 -0400

    Some uncommitted changes from before.

--- tock.js
+++ tock.js
@@ -1,6 +1,25 @@
 //----In JQuery----
 $(document).ready(function(){
 
+  function scroller(el, time) {
+    $(el).transition({
+      x: '-50%'
+      }, time, linear, function(){
+      $(el).css({ x:0 });
+      scroller(el, time);
+      });
+  }
+
+  var tF = $('#topFront');
+  var tB = $('#topBack');
+  var bF = $('#bottomFront');
+  var bB = $('#bottomBack');
+  
+  scroller(tF, 5000);
+  scroller(tB, 5000);
+  scroller(bF, 7000);
+  scroller(bB, 7000);
+
 //--LOAD RSS INTO SCROLLS--
 
 /*TO TRY
