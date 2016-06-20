f5a9b34fbb82da83bf9e2c2d441e50d7ff65c5bb
Alex <admin@QAWSEDRFTGYHUJIKOLP.home>
2016-06-19 21:37:01 -0400

    Uncommitted changes from previous session.

---
+++ feedAPI.js
@@ -0,0 +1,46 @@
+var http = require("http");
+    url = "https://www.google.com/jsapi";
+
+// get is a simple wrapper for request()
+// which sets the http method to GET
+var request = http.get(url, function (response) {
+    // data is streamed in chunks from the server
+    // so we have to handle the "data" event
+    var buffer = "",
+        data,
+        route;
+
+    response.on("data", function (chunk) {
+        buffer += chunk;
+    });
+
+    response.on("end", function (err) {
+        // finished transferring data
+        // dump the raw data
+        console.log(buffer);
+        console.log("\n");
+        data = JSON.parse(buffer);
+        route = data.routes[0];
+
+    });
+});
+
+//-------------------------------------------------------------
+
+google.load("feeds", "1");
+
+function initialize() {
+  var feed = new google.feeds.Feed("http://fastpshb.appspot.com/feed/1/fastpshb");
+  feed.load(function(result) {
+    if (!result.error) {
+      var container = document.getElementById("feed");
+      for (var i = 0; i < result.feed.entries.length; i++) {
+        var entry = result.feed.entries[i];
+        var div = document.createElement("div");
+        div.appendChild(document.createTextNode(entry.title));
+        container.appendChild(div);
+      }
+    }
+  });
+}
+google.setOnLoadCallback(initialize);
--- tock.js
+++ tock.js
@@ -1,8 +1,57 @@
-//----In JQuery----
-$(document).ready(function(){
-	function stop(){
-		$(this).css('-webkit-animation-duration', '0s');
+var bite = { data: [
+	{title: 'News Article1',
+	 author: 'Arthur M. Journalist',
+ 	 published: '03-05-2016'},
+
+	{title: 'News Article2',
+ 	 author: 'Arthur M. Journalist',
+   published: '03-07-2016'},
+
+	{title: 'News Article3',
+	 author: 'Arthur M. Journalist',
+	 published: '03-09-2016'}
+]
+};
+
+var bite1 = { data: [
+	{title: 'News Article4',
+	 author: 'Arthur M. Journalist',
+ 	 published: '03-05-2016'},
+
+	{title: 'News Article5',
+ 	 author: 'Arthur M. Journalist',
+   published: '03-07-2016'},
+
+	{title: 'News Article6',
+	 author: 'Arthur M. Journalist',
+	 published: '03-09-2016'}
+]
+};
+
+var posts = document.getElementsByClassName('bite');
+
+for( var i = 0 ; i < posts.length ; i++){
+	posts[i].innerHTML = bite.data[i%3].title;
+};
+
+var load  = setInterval(function(){loader1()}, 5000);
+var load2 = setInterval(function(){loader2()}, 5001);
+
+function loader1(){
+	if(posts[0].innerHTML === bite.data[0].title){
+		for( var i = 0 ; i < posts.length ; i++){
+			posts[i].innerHTML = bite1.data[i%3].title;
+		};
 	}
+}
+
+function loader2(){
+	if(posts[0].innerHTML === bite1.data[0].title){
+		for( var i = 0 ; i < posts.length ; i++){
+			posts[i].innerHTML = bite.data[i%3].title;
+		};
+	}
+}
 
 
 //--LOAD RSS INTO SCROLLS--
@@ -16,5 +65,3 @@ $(document).ready(function(){
 */
 
 //--MESS WITH SIZING--
-
-});
