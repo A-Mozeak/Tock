a8af6d92931670e1793f72d93d5b5a6b972ff27d
Alex M <alex.mozeak@gmail.com>
2016-08-05 11:29:44 -0400

    Organizational updates.

--- tock.js
+++ tock.js
@@ -10,56 +10,6 @@ if (typeof Object.create !== 'function') {
 var screenHeight = window.screen.availHeight;
 window.moveTo(0, screenHeight - 70);
 
-//So here I've made two JSON objects that contain smaller JSON bites of news data.
-var bite = { data: [
-	{title: 'News Article1',
-	 author: 'Arthur M. Journalist',
- 	 published: '03-05-2016'},
-
-	{title: 'News Article2',
- 	 author: 'Arthur M. Journalist',
-   published: '03-07-2016'},
-
-	{title: 'News Article3',
-	 author: 'Arthur M. Journalist',
-	 published: '03-09-2016'}
-]
-};
-
-var bite1 = { data: [
-	{title: 'News Article4',
-	 author: 'Arthur M. Journalist',
- 	 published: '03-05-2016'},
-
-	{title: 'News Article5',
- 	 author: 'Arthur M. Journalist',
-   published: '03-07-2016'},
-
-	{title: 'News Article6',
-	 author: 'Arthur M. Journalist',
-	 published: '03-09-2016'}
-]
-};
-
-//DELICIOUS BITE PROTOTYPES HERE 😁
-
-/*var data = '/sampleJSON.json'; //= the Feedly response 📥
-
-var Bite = {
-	create: (title, author, date, summary) => {
-		var instance = Object.create(Bite);
-		instance.title = title;
-		instance.author = author;
-		instance.date = date;
-		instance.summary = summary;
-		return instance
-	},
-	sayTitle: () => console.log(title),
-	sayAuthor: () => console.log(author)
-}*/
-
-//var item = Bite.create(data.title, data.author, data.published, data.summary.content);
-
 
 //Get the bite nodes to paste news data. 📠
 var posts = document.getElementsByClassName('bite');
@@ -90,6 +40,21 @@ function loader2(){
 };
 
 
+
+var querify = function(url){
+	var querified = url.replace(':', '%3A').replace(/\//g, '%2F');
+    return querified;
+};
+
+
+/*//---SUBSCRIPTION GETTER---
+var feedSearch = function(query){
+
+}
+*/
+// SANDBOX TOKEN ENDS 08/01 curl 'http://cloud.feedly.com/v3/mixes/contents?streamId=feed%2Fhttp%3A%2F%2Fwww.engadget.com%2Frss.xml&count=3' -H 'Authorization: OAuth 1QA6I3662OW2KEG48WA6' | python -m json.tool
+
+//---XHRs---
 var getResults = function(){
   var terms = document.getElementById('search').value;
   var xhr = new XMLHttpRequest;
@@ -112,29 +77,32 @@ var getResults = function(){
   xhr.send(null);
 };
 
+//---Window Actions---
+
+
+//---Click Events---
 document.getElementById('searchBtn').addEventListener('click', getResults);
 
-var querify = function(url){
-	var querified = url.replace(':', '%3A').replace(/\//g, '%2F');
-    return querified;
+
+//---Animation Events---
+
+//---Engine---
+		//Function to alternate displayed data at interval. 🔄
+var load  = setInterval(function(){ loader1() }, 5000);
+var load2 = setInterval(function(){ loader2() }, 10000);
+
+function loader1(){
+	if(posts[0].innerHTML === bite.data[0].title){
+		for( var i = 0 ; i < posts.length ; i++){
+			posts[i].innerHTML = bite1.data[i%3].title;
+		};
+	}
 };
 
-/*//---SUBSCRIPTION GETTER---
-var feedSearch = function(query){
-
-}
-*/
-// SANDBOX TOKEN ENDS 08/01 curl 'http://cloud.feedly.com/v3/mixes/contents?streamId=feed%2Fhttp%3A%2F%2Fwww.engadget.com%2Frss.xml&count=3' -H 'Authorization: OAuth 1QA6I3662OW2KEG48WA6' | python -m json.tool
-
-
-//--LOAD RSS INTO SCROLLS--
-
-/*TO TRY
-	Feedly API
-	Google AJAX
-	Other AJAX feeders:
-		Yahoo Pipes
-		App.net API
-*/
-
-//--MESS WITH SIZING--
+function loader2(){
+	if(posts[0].innerHTML === bite1.data[0].title){
+		for( var i = 0 ; i < posts.length ; i++){
+			posts[i].innerHTML = bite.data[i%3].title;
+		};
+	}
+};
